6b84a747e1aeb32890298ccd207b03469f80c084
per-sever <a.petrin107@gmail.com>
2022-10-16 14:51:24 +0300

    list auto-update was added

--- app/src/main/java/com/example/shopinglisttraining/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shopinglisttraining/domain/ShopListRepository.kt
@@ -1,11 +1,13 @@
 package com.example.shopinglisttraining.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopListRepository {
     fun addItem(item: ShopItem)
 
     fun removeItem(item: ShopItem)
 
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 
     fun editItem(item: ShopItem)
 
